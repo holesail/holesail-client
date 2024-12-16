84ccaad476d1da06f20639ce2fad2e3412071239
supersuryaansh <prod.genx@gmail.com>
2024-12-16 19:52:48 +0530

    Clean up

--- index.js
+++ index.js
@@ -1,96 +1,96 @@
 // Importing required modules
-const HyperDHT = require('hyperdht') // HyperDHT module for DHT functionality
-const net = require('net') // Net module for creating network clients and servers
+const HyperDHT = require("hyperdht"); // HyperDHT module for DHT functionality
+const net = require("net"); // Net module for creating network clients and servers
 
-const libNet = require('@holesail/hyper-cmd-lib-net') // Custom network library
-const b4a = require('b4a')
+const libNet = require("@holesail/hyper-cmd-lib-net"); // Custom network library
+const b4a = require("b4a");
 
 class holesailClient {
-  constructor (key, secure) {
+  constructor(key, secure) {
     // check if secure flag is enabled
-    if (secure === 'secure') {
-      this.secure = true
-      this.peerKey = HyperDHT.keyPair(b4a.from(key, 'hex'))
-      this.dht = new HyperDHT({ keyPair: this.peerKey })
+    if (secure === "secure") {
+      this.secure = true;
+      this.peerKey = HyperDHT.keyPair(b4a.from(key, "hex"));
+      this.dht = new HyperDHT({ keyPair: this.peerKey });
     } else {
-      this.peerKey = key
-      this.dht = new HyperDHT()
+      this.peerKey = key;
+      this.dht = new HyperDHT();
     }
 
-    this.stats = {}
-    this.proxy
+    this.stats = {};
+    this.proxy;
   }
 
-  connect (options, callback) {
+  connect(options, callback) {
     if (!options.udp) {
-      this.handleTCP(options, callback)
+      this.handleTCP(options, callback);
     } else {
-      this.handleUDP(options, callback)
+      this.handleUDP(options, callback);
     }
   } // end connect
 
   // Handle TCP connections
-  handleTCP (options, callback) {
+  handleTCP(options, callback) {
     this.proxy = net.createServer({ allowHalfOpen: true }, (c) => {
       return libNet.connPiper(
         c,
         () => {
-          let stream
+          let stream;
           if (this.secure) {
             stream = this.dht.connect(
-              Buffer.from(this.peerKey.publicKey, 'hex'),
-              { reusableSocket: true }
-            )
+              Buffer.from(this.peerKey.publicKey, "hex"),
+              { reusableSocket: true },
+            );
           } else {
-            stream = this.dht.connect(Buffer.from(this.peerKey, 'hex'), {
-              reusableSocket: true
-            })
+            stream = this.dht.connect(Buffer.from(this.peerKey, "hex"), {
+              reusableSocket: true,
+            });
           }
-          return stream
+          return stream;
         },
         { compress: false },
-        this.stats
-      )
-    })
+        this.stats,
+      );
+    });
 
-    const targetHost = options.address || '127.0.0.1'
+    const targetHost = options.address || "127.0.0.1";
     this.proxy.listen(options.port, targetHost, () => {
-      if (typeof callback === 'function') {
-        callback()
+      if (typeof callback === "function") {
+        callback();
       }
-    })
+    });
   }
 
   // Handle UDP connections
-  handleUDP (options, callback) {
-    let conn
+  handleUDP(options, callback) {
+    let conn;
 
     if (this.secure) {
-      conn = this.dht.connect(Buffer.from(this.peerKey.publicKey, 'hex'))
+      conn = this.dht.connect(Buffer.from(this.peerKey.publicKey, "hex"));
     } else {
-      conn = this.dht.connect(Buffer.from(this.peerKey, 'hex'))
+      conn = this.dht.connect(Buffer.from(this.peerKey, "hex"));
     }
 
-    conn.once('open', function () {
+    conn.once("open", function () {
       const handleUDP = libNet.udpPiper(conn, () => {
         return libNet.udpConnect({
           port: options.port || 8989,
-          host: options.address || '127.0.0.1',
-          bind: true
-        })
-      })
+          host: options.address || "127.0.0.1",
+          bind: true,
+        });
+      });
 
-      if (typeof callback === 'function') {
-        callback()
+      if (typeof callback === "function") {
+        callback();
       }
-    })
+    });
   }
 
-  destroy () {
-    this.dht.destroy()
-    this.proxy.close()
-    return 0
+  destroy() {
+    this.dht.destroy();
+    this.proxy.close();
+    return 0;
   }
 } // end client class
 
-module.exports = holesailClient
+module.exports = holesailClient;
--- test.js
+++ test.js
@@ -1,18 +1,11 @@
-const Holesailclient = require('./index.js')
+const Holesailclient = require("./index.js");
 
 const test1 = new Holesailclient(
-  'd8afd2605893ba587ffdc60044aa51ede164dbb71219d807ef55d624d8d09241',
-  'secure'
-)
-test1.connect({ port: 10000, address: '0.0.0.0', udp: true }, () => {
-  console.log('Connected UDP')
-  console.log('Running on 0.0.0.0:10000')
-})
+  "d8afd2605893ba587ffdc60044aa51ede164dbb71219d807ef55d624d8d09241",
+  "secure",
+);
+test1.connect({ port: 10000, address: "0.0.0.0", udp: true }, () => {
+  console.log("Connected UDP");
+  console.log("Running on 0.0.0.0:10000");
+});
 
-const test2 = new Holesailclient(
-  'b670936147a75633bd786d6c4e45c3e0965ce780b201b746adb9fc840b8d5a24'
-)
-test2.connect({ port: 8989, address: '127.0.0.1', udp: false }, () => {
-  console.log('Connected TCP')
-  console.log('Running on 127.0.0.1:8989')
-})
